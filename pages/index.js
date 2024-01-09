e7048e3fc3f91e9aa0634041bd65f2b6bcb3dade
Catalina <samplecatalina@gmail.com>
2024-01-08 22:26:22 -0500

    added feature: showing cover image on 'my posts' home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
 import { useState, useEffect } from 'react';
-import { API } from 'aws-amplify';
+import { API, Storage } from 'aws-amplify';
 import { listPosts } from '../src/graphql/queries';
 import Link from 'next/link';
 
 export default function Home() {
+  
   const [posts, setPosts] = useState([]);
+  
   useEffect(() => {
     fetchPosts();
   }, []);
+
   async function fetchPosts() {
     const postData = await API.graphql({ query: listPosts });
-    setPosts(postData.data.listPosts.items);
+
+    const { items } = postData.data.listPosts;
+    const postWithImages = await Promise.all(
+      items.map(async (post) => {
+        if (post.coverImage) {
+          post.coverImage = await Storage.get(post.coverImage);
+        }
+        return post;
+      })
+    );
+
+    setPosts(postWithImages);
   }
   return (
     <div>
@@ -20,9 +34,18 @@ export default function Home() {
       {
         posts.map((post, index) => (
           <Link key={index} href={`/posts/${post.id}`}>
-            <div className="cursor-pointer border-b border-gray-300 mt-8 pb-4">
-              <h2 className="text-sky-400 font-semibold" key={index}>{post.title}</h2>
-              <p className="text-gray-500 mt-2">User: {post.username}</p>
+            <div className='my-6 pb-6 border-b border-gray-300'>
+            {post.coverImage && (
+              <img
+              src={post.coverImage}
+              className='w-36 h-36 bg-contain bg-center 
+               rounded-full sm:mx-0 sm:shrink-0'
+              />
+            )}
+              <div className="cursor-pointer border-b border-gray-300 mt-8 pb-4">
+                <h2 className="text-sky-400 font-semibold" key={index}>{post.title}</h2>
+                <p className="text-gray-500 mt-2">User: {post.username}</p>
+              </div>
             </div>
           </Link>
         ))
